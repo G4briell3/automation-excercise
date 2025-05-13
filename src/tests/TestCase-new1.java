937e587ebfe200dda05339116457f8790d748581
G4briell3 <Gabriela.Diana.Surdu@gmail.com>
2025-05-13 10:03:51 +0300

    Update and rename TestCase-new1 to TestCase-new1.java

--- src/tests/TestCase-new1.java
+++ src/tests/TestCase-new1.java
@@ -7,5 +7,14 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 
 import TestData.URL;
-
+pulic class PersistentClassActions extends BaseTest
+{
+@Before @Override
+  public void before()
+  {
+    super.before();
+    driver.get(URL.MainPage);
+    mainPage.checkIconsOnPage();
+  }
+  
 
